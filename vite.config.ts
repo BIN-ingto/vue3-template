a707585bd6dd791b7335aeaba87b58864a8f4937
SviAnL <402661663@qq.com>
2024-08-27 15:18:09 +0800

    import base css and  update other config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue()]
+// https://cn.vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd())
+	console.log('env', env)
+	return {
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, 'src')
+			},
+			extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+		}
+	}
 })
